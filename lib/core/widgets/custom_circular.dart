e665212e80a10a7fe0cda8935666490bd19c5d4f
Bassam <103205476+Bassam-Jawish@users.noreply.github.com>
2024-05-20 16:44:34 +0000

    edit for privacy

--- lib/core/widgets/custom_circular.dart
+++
@@ -1,6 +0,0 @@
-
-import 'package:flutter/material.dart';
-
-Widget circularLoading() {
-  return const CircularProgressIndicator();
-}
--- lib/features/categories/domain/repository/category_repo.dart
+++
@@ -1,8 +0,0 @@
-import '../../../../core/resources/data_state.dart';
-import '../entities/choose_type_entity.dart';
-
-abstract class CategoryRepository {
-  // API methods
-  Future<DataState<dynamic>> getCategoryChildren(
-      dynamic id, int limit, int page, dynamic language);
-}
